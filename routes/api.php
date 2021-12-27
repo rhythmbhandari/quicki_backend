2d7fd523d5d8eb4029c0fd27875eed7beb157f5c
Rhythm Bhandari <rhythmbhandari@outlook.com>
2021-12-27 17:25:54 +0545

    payment from booking api

--- routes/api.php
+++ routes/api.php
@@ -79,6 +79,7 @@ Route::group(['as' => 'api.', 'middleware' => 'auth:api'], function ($router) {
   
   $router->post('/booking/create', [BookingController::class, 'store'])->name('booking.store');
   $router->post('/booking/change_status', [BookingController::class, 'change_status'])->name('booking.change_status');
+  $router->get('/booking/{booking_id}/payment', [PaymentController::class, 'getPaymentFromBooking'])->name('user.booking.payment');     
   $router->get('/booking/{booking_id}', [BookingController::class, 'getBooking'])->name('user.booking.show');         ///TO BE 
   $router->get('/user/booking/active', [BookingController::class, 'getActiveUserBooking'])->name('user.booking.active');
   $router->get('/rider/booking/active', [BookingController::class, 'getActiveRiderBooking'])->name('rider.booking.active');
